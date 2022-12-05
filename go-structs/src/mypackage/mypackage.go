717e598e1b46bc7bddd2cc4236de7ff13bdbf0f7
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2022-12-05 14:31:37 -0500

    estudian go

--- go-structs/src/mypackage/mypackage.go
+++ go-structs/src/mypackage/mypackage.go
@@ -19,3 +19,5 @@ func PrintMessage(text string) {
 func printMessagel(text string) {
 	fmt.Println("Mensaje publico")
 }
+
+//https://teams.microsoft.com/l/meetup-join/19%3ameeting_YzkzMDgyODUtZmE5Ny00YjYzLWIxNTYtZTU5NDZlOTNmOWQ1%40thread.v2/0?context=%7b%22Tid%22%3a%22bf208dcb-97e8-4d43-bd72-323680bef25c%22%2c%22Oid%22%3a%22f8dc5a02-9a64-4e03-a4bd-445fa271eb43%22%7d
